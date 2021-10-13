59ea365a7ff6b792f3ab45570a97f507042c346f
Chavin30 <martingaytan.lugo@gmail.com>
2021-10-12 19:05:30 -0500

    [IMP] primer carrousel para instlaciones

--- resources/views/components/main/instalaciones.blade.php
+++ resources/views/components/main/instalaciones.blade.php
@@ -1,10 +1,17 @@
-<div id='instalaciones' class="h-screen w-full flex items-center text-center">
-     <div class="grid h-screen grid-cols-1 md:grid-cols-2 w-full max-w-7xl mx-auto px-2 py-2 sm:px-6 lg:px-8 border-2 shadow-xl">
-         <div class="w-full">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3652.461839034441!2d-103.94502036145882!3d18.928510979312282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2smx!4v1634070966901!5m2!1sen!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<div id='instalaciones' class="lg:h-screen w-full flex flex-col items-center text-center bg-white">
+     <div class="grid gap-x-3 gap-y-3 items-center w-11/12 h-5/6 grid-cols-1 lg:grid-cols-2  mx-auto px-2 sm:px-6 lg:px-8 bg-white" style="margin-top: 45px;">
+        <div class="col-span-2 w-full">
+            hola
+        </div>
+         <div class="w-full flex-col flex justify-center mt-5 md:mt-0">
+            <x-carrousels.instalaciones/>
          </div>
-         <div>
-
+         <div class="w-full flex justify-center mt-5 md:mt-0">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d10330.602219556691!2d-103.95824991094457!3d18.930450285672755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!3e0!4m3!3m2!1d18.9357418!2d-103.9646774!4m3!3m2!1d18.928403799999998!2d-103.944204!5e1!3m2!1sen!2smx!4v1634078735304!5m2!1sen!2smx" width="100%" height="500px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
          </div class="w-full">
-     </div>
+    </div>
 </div>
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,17 +19,52 @@
 
         <style>
             html{
-            scroll-behavior: smooth; 
+            scroll-behavior: smooth;
+            }
+
+            .carousel-open:checked+.carousel-item {
+            position: static;
+            opacity: 100;
+            }
+
+            .carousel-item {
+            -webkit-transition: opacity 0.6s ease-out;
+            transition: opacity 0.6s ease-out;
+            }
+
+            #carousel-1:checked~.control-1,
+            #carousel-2:checked~.control-2,
+            #carousel-3:checked~.control-3 {
+            display: block;
+            }
+
+            .carousel-indicators {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            bottom: 2%;
+            left: 0;
+            right: 0;
+            text-align: center;
+            z-index: 10;
+            }
+
+            #carousel-1:checked~.control-1~.carousel-indicators li:nth-child(1) .carousel-bullet,
+            #carousel-2:checked~.control-2~.carousel-indicators li:nth-child(2) .carousel-bullet,
+            #carousel-3:checked~.control-3~.carousel-indicators li:nth-child(3) .carousel-bullet {
+            color: #2b6cb0;
+            /*Set to match the Tailwind colour you want the active one to be */
             }
         </style>
     </head>
     <body class="font-sans antialiased">
 
         <!--<div class="min-h-screen bg-gray-100">-->
-        <div class="bg-gray-100" 
-            x-data="{active:'inicio'}" 
-            @scroll.window="active = document.querySelector('#producto').getBoundingClientRect().top <= 300 ? 'producto' : 
-            (document.querySelector('#restaurante').getBoundingClientRect().top <= 300 ? 'restaurante': 
+        <div class="bg-gray-100"
+            x-data="{active:'inicio'}"
+            @scroll.window="active = document.querySelector('#producto').getBoundingClientRect().top <= 300 ? 'producto' :
+            (document.querySelector('#restaurante').getBoundingClientRect().top <= 300 ? 'restaurante':
             (document.querySelector('#instalaciones').getBoundingClientRect().top <= 300 ? 'instalaciones' : 'inicio'))">
             <x-header.main/>
             <x-header.menu/>
